5600e3d953327adebe92e65c9ffb92e8493c03aa
René Bärnreuther <rene.baernreuther@web.de>
2025-12-30 14:10:15 +0100

    User werden in der Datenbank (lokale H2) gespeichert

--- src/main/java/de/baernreuther/dart/security/SecurityConfig.java
+++ src/main/java/de/baernreuther/dart/security/SecurityConfig.java
@@ -5,30 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("board1")
-                .password("test")
-                .roles("player")
-                .build();
-
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("board2")
-                .password("test")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user1);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
